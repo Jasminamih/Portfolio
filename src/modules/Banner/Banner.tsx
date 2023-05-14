f14bc3ce6c5379b0dad305b3961ca5b48063c3d2
Jasmina Mihajloska <mihajloska.jasmina@outlook.com>
2023-05-14 17:39:53 +0200

    update

--- src/modules/Banner/Banner.tsx
+++ src/modules/Banner/Banner.tsx
@@ -1,11 +1,10 @@
 import React, { FC } from 'react'
 import styles from './Banner.module.scss'
 import EarthCanvas from '../../elements/canvas/Moon/Moon'
-import { F_Baskervville, F_Open_Sans } from '@/fonts'
 
 const Banner: FC = () => {
   return (
-    <div id='home' className={`${styles.banner} ${F_Open_Sans.className}`}><div><h3>Hi, my name is</h3>
+    <div id='home' className={`${styles.bannerWrapper}`}><div><h3>Hi, my name is</h3>
     <h1>Jasmina Mihajloska</h1>
     <h2>A Front-End developer </h2>
    
--- typings.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'maath/random/dist/maath-random.esm'{
-    export function randomNumber(min: number, max: number): number;
-    export declare function inSphere(buffer: TypedArray, sphere?: Sphere, rng?: Generator): TypedArray;
-
-}
